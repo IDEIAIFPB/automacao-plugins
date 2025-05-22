3ddb1bfb69f00c4c5125c7b1423d3f2d20ed6adc
FranciscoNeto <francisco.neto.ext@synchro.com.br>
2025-05-22 14:34:52 -0300

    automacao-plugins#07 - formata o codigo

--- src/alimentacao.py
+++ src/alimentacao.py
@@ -1,10 +1,12 @@
 import xml.etree.ElementTree as ET
 import json
-import os, sys
+import os
+import sys
+
 
 def extract_properties_with_full_path(element, current_path="", collected=None):
     if collected is None:
-        arquivo = open('properties.json', 'r')
+        arquivo = open("properties.json", "r")
         collected = json.load(arquivo)
 
     for prop in element.findall("./property"):
@@ -24,13 +26,14 @@ def extract_properties_with_full_path(element, current_path="", collected=None):
                 if xml_prop is not None and xml_prop.get("xpath"):
                     if full_path not in collected:
                         collected[full_path] = xml_prop.get("xpath")
-            
+
         # Processa recursivamente subpropriedades
         subprops_container = prop.find("properties")
         if subprops_container is not None:
             extract_properties_with_full_path(subprops_container, full_path, collected)
 
     return collected
+
 
 # Inicia a extração a partir da raiz <properties>
 xml_file_path = sys.argv[1]
@@ -46,5 +49,5 @@ properties_root = root.find("properties")
 properties_dict = extract_properties_with_full_path(properties_root)
 
 # Exibe o resultado como JSON
-properties_json = open('properties.json', 'w+')
+properties_json = open("properties.json", "w+")
 properties_json.write(json.dumps(properties_dict, indent=2, ensure_ascii=False))
