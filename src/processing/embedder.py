2ed7700f39f03d55dfd8d3a2ab6e040f14d4c980
emanuelleGued <manugguedes1701@gmail.com>
2025-07-07 16:50:35 -0300

    mudando modelo utilizado no embedding

--- src/processing/embedder.py
+++ src/processing/embedder.py
@@ -1,7 +1,15 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("intfloat/multilingual-e5-base")
 
 
 def generate_embeddings(chunks):
+    """
+    Gera embeddings para uma lista de chunks de texto usando o modelo SentenceTransformer.
+    Args:
+        chunks (list): Uma lista de strings, onde cada string é um chunk de texto.
+    Returns:
+        numpy.ndarray: Um array NumPy contendo os embeddings para cada chunk.
+                       A forma do array será (número_de_chunks, dimensão_do_embedding).
+    """
     return model.encode(chunks)
