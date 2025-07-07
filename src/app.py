bcf6614bcda4b7f42693320192e941eb78bde5cd
emanuelleGued <manugguedes1701@gmail.com>
2025-07-07 16:49:46 -0300

    adicionando app para rodar o flask

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from controller.document_controller import document_bp
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.register_blueprint(document_bp, url_prefix="/api")
+
+
+@app.route("/")
+def health_check():
+    return "API RAG está online!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
