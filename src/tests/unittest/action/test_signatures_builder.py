86fc54fcab50a85a0b9d965a6d9541384a079094
MateusMorais <mateusdeassismorais@gmail.com>
2025-07-21 15:29:30 -0300

    automacao-plugins#40 - cria testes para SignaturesBuilder

--- src/tests/unittest/action/test_signatures_builder.py
+++ src/tests/unittest/action/test_signatures_builder.py
@@ -3,23 +3,31 @@ from unittest import TestCase, main
 import lxml.etree as etree
 
 from src.core.action.request import SignaturesBuilder
-from src.tests.utils import build_output_file_path, export_xml_to_file, get_xml
+from src.mapper import Mapper
+from src.tests.utils import build_output_file_path, get_xml
 
 
 class TestSignaturesBuilder(TestCase):
     def setUp(self):
         self._builder = SignaturesBuilder()
-        self._signatures = [
-            {"parent": "parent1", "target": "target1", "type": "type1"},
-            {"parent": "parent2", "target": "target2", "type": "type2"},
-        ]
-        self._output_file = build_output_file_path("signatures_builder_test.xml")
+        self._xsd_file = "resources/xsd-files/nfse-v2-04.xsd"
+        output_file = build_output_file_path("cli.xml")
+        self._final_envelope_tag = "GerarNfseEnvio"
+        parser = Mapper(self._final_envelope_tag, "teste", self._xsd_file, output_file)
+        parser.build()
+        self._signatures = parser._mapper_builder.metadata.signature
+        self._root = etree.Element("teste")
+        self._signatures_expected = """<teste>
+  <signatures>
+    <signature attribute="Id" parent="Rps" target="InfDeclaracaoPrestacaoServico" type="ELEMENT"/>
+  </signatures>
+</teste>
+"""
 
     def test_build(self):
-        root = etree.Element("root")
-        tree = self._builder.build(root, self._signatures)
+        tree = self._builder.build(self._root, self._signatures)
         s_tree = get_xml(tree)
-        export_xml_to_file(s_tree, self._output_file)
+        self.assertEqual(s_tree, self._signatures_expected)
 
 
 if __name__ == "__main__":
