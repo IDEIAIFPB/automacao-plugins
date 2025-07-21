2a7499679838612fab8a5e38e3b1877cde8adba8
MateusMorais <mateusdeassismorais@gmail.com>
2025-07-21 13:39:15 -0300

    automacao-plugins#40 - cria testes para StatusBuilder

--- src/tests/unittest/action/test_status_builder.py
+++ src/tests/unittest/action/test_status_builder.py
@@ -5,7 +5,7 @@ from xmlschema import XMLSchema
 
 from src.core.action.response import StatusBuilder
 from src.core.utils.constants import CONDITTIONS_MAP
-from src.tests.utils import build_output_file_path, export_xml_to_file, get_xml
+from src.tests.utils import build_output_file_path, get_xml
 
 
 class TestStatusBuilder(TestCase):
@@ -13,18 +13,34 @@ class TestStatusBuilder(TestCase):
         self._builder = StatusBuilder()
         self._output_file = build_output_file_path("status_builder_test.xml")
         self._schema = XMLSchema("resources/xsd-files/nfse-v2-02.xsd")
-
-    def test_build(self):
-        root = etree.Element("teste")
-        response_tag = self._schema.elements.get("CancelarNfseResposta")
-        targets_tags = {
+        self._root = etree.Element("teste")
+        self._response_tag = self._schema.elements.get("CancelarNfseResposta")
+        self._targets_tags = {
             "data_hora": "DataHora",
             "codigo_cancelamento": "Codigo",
         }
-        file_type = "cancelamento"
-        tree = self._builder.build(file_type, root, CONDITTIONS_MAP, response_tag, targets_tags)
+        self._file_type = "cancelamento"
+        self._status_str = """<teste>
+  <status>
+    <cancelled>
+      <conditions>
+        <condition comparison="EXISTS" xpath="/CancelarNfseResposta/RetCancelamento/NfseCancelamento/Confirmacao/DataHora"/>
+      </conditions>
+    </cancelled>
+    <rejected>
+      <conditions>
+        <condition comparison="EXISTS" xpath="/CancelarNfseResposta/ListaMensagemRetorno/MensagemRetorno/Codigo"/>
+      </conditions>
+    </rejected>
+    <default status="UNKNOWN"/>
+  </status>
+</teste>
+"""
+
+    def test_build(self):
+        tree = self._builder.build(self._file_type, self._root, CONDITTIONS_MAP, self._response_tag, self._targets_tags)
         s_tree = get_xml(tree)
-        export_xml_to_file(s_tree, self._output_file)
+        self.assertEqual(s_tree, self._status_str)
 
 
 if __name__ == "__main__":
