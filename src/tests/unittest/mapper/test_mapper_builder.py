defaadd0e58830de66a1b6e8d598e69dccf7ddce
Francisco <franbezerran@gmail.com>
2025-07-28 13:59:21 -0300

    automacao-plugins#39 - corrige adição de arquivos de resources

--- src/tests/unittest/mapper/test_mapper_builder.py
+++ src/tests/unittest/mapper/test_mapper_builder.py
@@ -3,17 +3,23 @@ from unittest import TestCase, main
 import xmlschema
 
 from src.core.mapper import MapperBuilder
+from src.core.utils.xml_utils import get_xml
 
 
 class TestMapperBuilder(TestCase):
     def setUp(self):
         self._builder = MapperBuilder()
-        self._schema = xmlschema.XMLSchema("resources/xsd-files/nfse-v2-02.xsd")
-        self._root = self._schema.elements.get("EnviarLoteRpsEnvio")
-        self._plugin_id = "emissao-itaperuna"
+        self._schema = xmlschema.XMLSchema("resources/xsd-files/nfse-v2-04.xsd")
+        self._root = self._schema.elements.get("GerarNfseEnvio")
+        self._plugin_id = "emissao-teste"
+        with open("src/tests/resources/mapper.xml") as file:
+            self._emissao_tree = file.read().replace("\n", "")
 
     def test_build(self):
-        pass
+        xml = self._builder.build(self._plugin_id, self._root)
+
+        with open("teste.xml", "w") as file:
+            file.write(get_xml(xml))
 
 
 if __name__ == "__main__":
